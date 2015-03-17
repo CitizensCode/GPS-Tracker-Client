24c5932d0d59a185abf0f3fd79fbf1195f3443e6
Sam Stiles <samuel.dev.stiles@gmail.com>
2015-03-17 10:19:10 -0300

    Last nights uncommitted work

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ var express = require('express');
 var serveStatic = require('serve-static');
 var app = express()
   .use(serveStatic('public'))
-  .listen(1337);
+  .listen(1337, function () {
+    console.log('Static file server listening on *:1337');
+  });
--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,23 @@
-L.mapbox.accessToken = 'pk.eyJ1Ijoic2FtdWVsZGV2IiwiYSI6IjFRV3VPMEEifQ.xqZJCVD5H6955kQPYQ9ZEQ';
-var map = L.mapbox.map('map', 'samueldev.lf9kkbcm')
-  .setView([40, -74.50], 9);
+(function () {
+  'use strict';
+
+  var socket = io('http://localhost:1338/');
+
+  L.mapbox.accessToken = 'pk.eyJ1Ijoic2FtdWVsZGV2IiwiYSI6IjFRV3VPMEEifQ.xqZJCVD5H6955kQPYQ9ZEQ';
+  var map = L.mapbox.map('map', 'samueldev.lf9kkbcm')
+    .setView([45.944921, -66.687651], 14);
+
+  var bus = L.marker([-73, 40], {
+    icon: L.mapbox.marker.icon({
+      'marker-color': '#f86767',
+      'marker-symbol': 'bus'
+    })
+  });
+
+  socket.on('bus coord', function (location) {
+    console.info('Received new bus coordinate from the API. Updating marker.', location);
+    bus.setLatLng(L.latLng(location.lat, location.lng));
+  });
+
+  bus.addTo(map);
+})();
