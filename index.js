1469d608424ca26f5155b315099b04e7a37edf22
Sam Stiles <samuel.dev.stiles@gmail.com>
2015-03-18 15:35:49 -0300

    Adjust port for heroku

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var serveStatic = require('serve-static');
 var app = express()
   .use(serveStatic('public'))
-  .listen(1337, function () {
-    console.log('Static file server listening on *:1337');
+  .listen(process.env.PORT, function () {
+    console.log('Static file server listening on *:' + process.env.PORT);
   });
